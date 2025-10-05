2146e073f3c0c9d2eed14495f598943a4b7fbb44
MXP1000 <bhanitadutta554@gmail.com>
2025-10-05 19:08:01 +0530

    BuildVars.java

--- src/main/java/se/anyro/userinfobot/BuildVars.java
+++ src/main/java/se/anyro/userinfobot/BuildVars.java
@@ -1,6 +1,6 @@
 package se.anyro.userinfobot;
 
 public class BuildVars {
-    public static final int OWNER = // Your user id here. Send message to @userinfobot to get it.
-    public static final String TOKEN = // Your bot token here. Use @BotFather.
+    public static final int OWNER = 8478834931// Your user id here. Send message to @userinfobot to get it.
+    public static final String TOKEN = 7538024808:AAE5qmmbUAS5j5_f-Oe7Ev2gBRHz03m9uTU// Your bot token here. Use @BotFather.
 }
